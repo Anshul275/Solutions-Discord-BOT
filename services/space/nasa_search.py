045fbe6c8e764d9f8e6c54af308b1a6872e01d76
Anshul Pandey <anshulpandey275@gmail.com>
2021-04-03 10:36:32 +0530

    Feature to search for related NASA News and Images

---
+++ services/space/nasa_search.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import random
+
+
+def search_nasa(url, search):
+    query = {'q': search}
+    response = requests.get(url, params = query)
+    json_data = json.loads(response.text)
+
+    image_list = json_data['collection']['items']
+    
+    nasa_text = ""
+    nasa_image = ""
+    if len(image_list) >= 1:
+        image = random.choice(image_list)
+        nasa_text = image['data'][0]['description'] + "\n"
+        nasa_image = image['links'][0]['href']
+    else:
+        nasa_image = "```No related images from NASA........```\n"
+
+    return nasa_text, nasa_image
+
+
+def nasa_image(search):
+    mainSite = "https://images-api.nasa.gov/"
+    endpoint = "/search"
+
+    url = mainSite + endpoint
+    nasa_text = ""
+    nasa_image = ""
+
+    try:
+        nasa_text, nasa_image = search_nasa(url, search)
+    except:
+        print("Server doesn't respond to requests........")
+
+    return nasa_text, nasa_image
